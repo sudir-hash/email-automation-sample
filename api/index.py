7ba6d05e76666250e47f531eec72d529ad82b3d3
Prakash Ramanathan <prashprakash1211@gmail.com>
2023-07-07 09:55:34 +0530

    added vercel

--- api/index.py
+++ api/index.py
@@ -5,22 +5,9 @@ from email.mime.base import MIMEBase
 from email import encoders
 from flask import Flask, render_template, Response, request, redirect
 import os.path
-# try:
-#     from flask.ext.cors import CORS  # The typical way to import flask-cors
-# except ImportError:
-#     # Path hack allows examples to be run without installation.
-#     import os
-#     parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#     os.sys.path.insert(0, parentdir)
 
-#     from flask.ext.cors import CORS
 
 app = Flask(__name__)
-
-# cors = CORS(app)
-# @app.route('/',methods=["GET"])
-# def start():
-#     return "hi"
 
 
 
